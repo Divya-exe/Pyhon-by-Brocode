ba82d176d5e3f4be0fc7c1f58e509e2d68d04035
divya.jadav2005@gmail.com <divya.jadav2005@gmail.com>
2026-02-01 23:25:10 +0530

    adding Multi level inheritance

---
+++ multilevel_inheritance.py
@@ -0,0 +1,31 @@
+class Animal:
+    def __init__(self,name):
+        self.name=name
+        
+    def sleep(self):
+        print(f"{self.name} is slepping")
+        
+class Prey(Animal):
+    def flee(self):
+        print("This animal is Fleeing")
+        
+
+class Predator(Animal):
+    def hunt(self):
+        print("This animal is hunting")
+
+class Rabbit(Prey):
+    pass
+
+class Hawk(Predator):
+    pass
+
+class Fish(Prey, Predator):
+    pass
+
+rabbit= Rabbit("little")
+hawk = Hawk("hose")
+fish = Fish("luna")
+
+
+fish.hunt()
