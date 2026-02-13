fd4f565eefad4c330e9ecdf65b4d38ad1c2a7294
divya.jadav2005@gmail.com <divya.jadav2005@gmail.com>
2026-02-13 22:46:50 +0530

    update code

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 #python file detection
 import os
-file_path = "D:\python by bro code\text.txt"
+file_path = "D:\\python by bro code\\text.txt"
 if os.path.exists(file_path):
     print(f"the loaction '{file_path}' exxist")
+    if os.path.isfile(file_path):
+        print("that if file")
+    
 else:
     print("not here")
