ca82a812a8d84f2094f969913b6b63f09dcccffa
divya.jadav2005@gmail.com <divya.jadav2005@gmail.com>
2026-02-14 15:39:22 +0530

    update code

--- reading_file.py
+++ reading_file.py
@@ -12,24 +12,39 @@
 # except PermissionError:
 #     print("you don't have permission")
 
-#<-----------------------json--------------------->
+#<-----------------------   json   --------------------->
 
 
 # for json file
 
-import json
-file_path ="text.json"
+# import json
+# file_path ="text.json"
+
+# try:
+#     with open(file_path, "r") as file:
+#         content =json.load(file)
+#         print(content)
+    
+# except FileNotFoundError:
+#   print("this file was not found")
+
+# except PermissionError:
+#     print("you don't have permission")
+
+#<-----------------------   CSV   --------------------->  
+
+import csv
+file_path ="text.csv"
 
 try:
     with open(file_path, "r") as file:
-        content =json.load(file)
-        print(content)
+        content =csv.reader(file)
+        for line in content:
+            print(line)
     
 except FileNotFoundError:
   print("this file was not found")
 
 except PermissionError:
     print("you don't have permission")
-
-
     
