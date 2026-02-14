084a4641772b2fe311c17f5e94fed48460a9cd0b
divya.jadav2005@gmail.com <divya.jadav2005@gmail.com>
2026-02-14 15:34:08 +0530

    update code

--- reading_file.py
+++ reading_file.py
@@ -1,13 +1,35 @@
-file_path ="text.txt"
+#for text file
+# file_path ="text.txt"
+
+# try:
+#     with open(file_path, "r") as file:
+#         content =file.read()
+#         print(content)
+    
+# except FileNotFoundError:
+#     print("this file was not found")
+    
+# except PermissionError:
+#     print("you don't have permission")
+
+#<-----------------------json--------------------->
+
+
+# for json file
+
+import json
+file_path ="text.json"
 
 try:
     with open(file_path, "r") as file:
-        content =file.read()
+        content =json.load(file)
         print(content)
     
 except FileNotFoundError:
-    print("this file was not found")
-    
+  print("this file was not found")
+
 except PermissionError:
     print("you don't have permission")
+
+
     
