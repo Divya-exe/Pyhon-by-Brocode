7e4b2a5835d0ab2b1a05948cc65ccf3f68c0d133
divya.jadav2005@gmail.com <divya.jadav2005@gmail.com>
2026-02-24 21:37:16 +0530

    update file

--- line_edits.py
+++ line_edits.py
@@ -1,10 +1,12 @@
 import sys 
-from PyQt5.QtWidgets import QApplication ,QMainWindow, QLineEdit, QPushButton, QRadioButton
+from PyQt5.QtWidgets import QApplication ,QMainWindow, QLineEdit, QPushButton, QRadioButton ,QLabel
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setGeometry(700, 300, 500, 500)
+        label = QLabel("Python_information_form", self)
+        label.setGeometry(10, 10, 250 , 40)
         self.line_edit = QLineEdit(self)
         self.email_edit= QLineEdit(self)
         self.psw_edit= QLineEdit(self)
@@ -15,15 +17,15 @@ class MainWindow(QMainWindow):
         self.initUI()
     
     def initUI(self):
-        self.line_edit.setGeometry(10, 10 , 300, 40)
-        self.email_edit.setGeometry(10, 60 , 300, 40)
-        self.psw_edit.setGeometry(10, 110, 300, 40)
+        self.line_edit.setGeometry(10, 50 , 300, 40)
+        self.email_edit.setGeometry(10, 100 , 300, 40)
+        self.psw_edit.setGeometry(10, 150, 300, 40)
         
-        self.radio1.setGeometry(0, 160, 300, 50)
-        self.radio2.setGeometry(100, 160, 300, 50)
-        self.radio3.setGeometry(230, 160, 300, 50)
+        self.radio1.setGeometry(0, 200, 300, 50)
+        self.radio2.setGeometry(100, 200, 300, 50)
+        self.radio3.setGeometry(230, 200, 300, 50)
         
-        self.button.setGeometry(10, 210, 100,40)
+        self.button.setGeometry(10, 250, 100,40)
         self.line_edit.setStyleSheet("font-size: 25px;"
                                      "font-family: Arial;")
         self.email_edit.setStyleSheet("font-size: 25px;"
